ea3498df3f83f6c2d56bc64f68365de94350adae
Sahil <issahil404@gmail.com>
2024-09-30 17:39:19 +0530

    added airbnb new fields

--- src/index.js
+++ src/index.js
@@ -23,9 +23,13 @@ async function main() {
     // logger.info(`Found ${dropboxJobs.length} Dropbox jobs with descriptions`);
     //
 
-    logger.info("Starting Slack job scraping");
-    const slackJobs = await scrapeSlackJobs(browser);
-    logger.info(`Found ${slackJobs.length} Slack jobs with descriptions`);
+    // logger.info("Starting Slack job scraping");
+    // const slackJobs = await scrapeSlackJobs(browser);
+    // logger.info(`Found ${slackJobs.length} Slack jobs with descriptions`);
+
+    logger.info("Starting Airbnb job scrapping");
+    const airbnbJobs = await scrapeAirbnbJobs(browser);
+    logger.info(`Found ${airbnbJobs.length} Airbnb jobs with descriptions`);
 
     // logger.info("Starting Paytm job scraping");
     // const paytmJobs = await scrapePaytmJobs(browser);
@@ -35,7 +39,8 @@ async function main() {
     let allJobs = [
       // ...paytmJobs,
       // ...dropboxJobs,
-      ...slackJobs,
+      // ...slackJobs,
+      ...airbnbJobs,
       // Add other job arrays here when uncommented
     ];
 
