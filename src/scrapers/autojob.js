8fa971bb3f55995d8c491a8ff02716f6a0bf009c
jambong-ralpher <jambongralpher@gmail.com>
2024-11-11 16:21:22 +0100

    re-adde error handlingd

--- src/scrapers/autojob.js
+++ src/scrapers/autojob.js
@@ -4,23 +4,33 @@ import puppeteer from 'puppeteer';
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  
-  await page.goto('https://automattic.com/work-with-us/');
+  try {
+    await page.goto('https://automattic.com/work-with-us/');
 
-  const jobDetails = await page.evaluate(() => {
-    const jobs = [];
-    document.querySelectorAll('.job-card').forEach(job => {
-      const title = job.querySelector('.job-card-header h3')?.innerText;
-      const link = job.href;
-      const category = job.querySelector('.job-card-category')?.innerText;
+    const jobDetails = await page.evaluate(() => {
+      const jobs = [];
+      const jobCards = document.querySelectorAll('.job-card');
 
-      jobs.push({ title, link, category });
+      if (jobCards.length === 0) {
+        throw new Error('No job listings found.');
+      }
+
+      jobCards.forEach(job => {
+        const title = job.querySelector('.job-card-header h3')?.innerText;
+        const link = job.href;
+        const category = job.querySelector('.job-card-category')?.innerText;
+
+        jobs.push({ title, link, category });
+      });
+
+      return jobs;
     });
-    return jobs;
-  });
 
-  console.log(`Found ${jobDetails.length} jobs in Automattic jobs page.`);
-  console.log(jobDetails);
-
-  await browser.close();
+    console.log(`Found ${jobDetails.length} jobs in Automattic jobs.`);
+    console.log(jobDetails);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  } finally {
+    await browser.close();
+  }
 })();
